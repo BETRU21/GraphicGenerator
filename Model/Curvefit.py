322a8bf7a2328ccc194eb1b090a8e67b1d65a148
Benjamin <benjamin.trudel.2@ulaval.ca>
2021-09-14 13:54:21 -0400

    Finish first version of Curvefit and start doing things in viewCurvefit

--- Model/Curvefit.py
+++ Model/Curvefit.py
@@ -5,27 +5,41 @@ from scipy.optimize import curve_fit
 class Curvefit:
 	def __init__(self):
 		self.functions = {}
+		self.functionsParam = {}
 		self.buildFunctions()
+		self.buildFunctionsParam()
 
 	# Public functions
 
 	def listFunctions(self):
-		self.functions.key()
+		"""List available functions."""
+		return self.functions.key()
 
 	def getFunction(self, key):
 		if type(key) is not str:
-			tyype = type(key)
-			string = f"key is not a string. key is {tyype}"
 			raise TypeError(string)
 		return self.functions.get(key)
 
+	def getAllFunction(self):
+		return self.functions
+
+	def getFunctionParam(self, key):
+		if type(key) is not str:
+			raise TypeError(string)
+		return self.functionsParam.get(key)
+
+	def getAllFunctionParam(self):
+		return self.functionsParam
+
 	def curvefit(self, dataX, dataY, function, bounds=False):
-		if type(dataX) is not list:
-			if type(dataX) is not np.ndarray:
-				raise TypeError("dataX is not a list or np.ndarray.")
-		if type(dataY) is not list:
-			if type(dataY) is not np.ndarray:
-				raise TypeError("dataY is not a list or np.ndarray.")
+		"""Curvefit a function with data.
+		Args:
+			dataX()
+		"""
+		if type(dataX) is not np.ndarray:
+			raise TypeError("dataX is not a np.ndarray.")
+		if type(dataY) is not np.ndarray:
+			raise TypeError("dataY is not a np.ndarray.")
 		if len(dataX) != len(dataY):
 			raise ValueError("data len are not equal.")
 
@@ -40,6 +54,7 @@ class Curvefit:
 	# Non-public functions
 
 	def buildFunctions(self):
+		# Create a method of each function to use it in curve_fit
 		self.functions["sinus"] = self.sinus
 		self.functions["cosinus"] = self.cosinus
 		self.functions["gaussian"] = self.gaussian
@@ -48,8 +63,14 @@ class Curvefit:
 		self.functions["polynomial2Degree"] = self.polynomial2Degree
 		self.functions["polynomial3Degree"] = self.polynomial3Degree
 
-	def exponential(self, X, a, b, c, d):
-		return a*np.exp(b*X-c)+d
+	def buildFunctionsParam(self):
+		self.functionsParam["sinus"] = "a*np.sin((X*b)+c)+d where popt=[a,b,c,d]"
+		self.functionsParam["cosinus"] = "a*np.cos((X*b)+c)+d where popt=[a,b,c,d]"
+		self.functionsParam["gaussian"] = "a*np.exp((-(b*X+c)**2))+d where popt=[a,b,c,d]"
+		self.functionsParam["exponential"] = "a*np.exp(b*X-c)+d where popt=[a,b,c,d]"
+		self.functionsParam["straightLine"] = "a*X + b where popt=[a,b]"
+		self.functionsParam["polynomial2Degree"] = "a*X**2 + b*x + c where popt=[a,b,c]"
+		self.functionsParam["polynomial3Degree"] = "a*X**3 + b*X**2 + c*X + d where popt=[a,b,c,d]"
 
 	def sinus(self, X, a, b, c, d):
 		return a*np.sin((X*b)+c)+d
@@ -59,6 +80,9 @@ class Curvefit:
 
 	def gaussian(self, X, a, b, c, d):
 		return a*np.exp((-(b*X+c)**2))+d
+
+	def exponential(self, X, a, b, c, d):
+		return a*np.exp(b*X-c)+d
 
 	def straightLine(self, X, a, b):
 		return a*X + b
--- View/ViewCurvefit.py
+++ View/ViewCurvefit.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QWidget
-from PyQt5 import uic
 from PyQt5 import QtGui
+from PyQt5 import uic
+import numpy as np
 import os
 
 MainWindowPath = os.path.dirname(os.path.realpath(__file__)) + '{}CurvefitWindow.ui'.format(os.sep)
@@ -13,3 +14,15 @@ class ViewCurvefit(QWidget, Ui_MainWindow):
         self.modelGraphic = modelGraphic
         self.modelData = modelData
         self.modelCurvefit = modelCurvefit
+
+    def test(self):
+        dataX = np.array([1,2,3,4,5,6,7,8,9])
+        dataY = np.array([0,2,4,6,8,10,12,14,16])
+
+
+        f = modelCurvefit.getFunction("straightLine")
+
+        line, popt = modelCurvefit.curvefit(dataX, dataY, f)
+
+        print(popt)
+        print(modelCurvefit.getFunctionParam("straightLine"))
